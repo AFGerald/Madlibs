56eab1809b1d3b31915ca41cb6742e9bf52aa992
AustinGerald <a.floydgerald@gmail.com>
2019-03-03 10:49:38 -0600

    put number labels on inputs

--- src/components/card.js
+++ src/components/card.js
@@ -3,43 +3,51 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '', 
+    adjectiveOne: '',
+    celebOne: '', 
+    adjectiveTwo: '',   
+    nounOne: '',
+    numberOne: '',
+    numberTwo: '',  
+    nounTwo: '',
+    adjectiveThree: '',
+    celecbTwo: '',
+    celebThree: '',
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+    contentVisible: false
+
+}
+
 class Card extends Component {
 
     constructor() {
         super()
 
 
-        this.state = {
-            color: '',
-            pluralNoun: '', 
-            adjectiveOne: '',
-            celebOne: '', 
-            adjectiveTwo: '',   
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',  
-            nounTwo: '',
-            adjectiveThree: '',
-            celecbTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFirve: '',
-            contentVisible: false
-        }
+         this.state = INITIAL_STATE;
         
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleInputSubmit = this.handleInputSubmit.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     handleInputChange(event) {
-         this.setState({ [event.target.name]: ' event.target.value' })
+         this.setState({ [event.target.name]: event.target.value })
     }
 
     handleFormSubmit(event) {
-        event.preventDefualt()
-        this.setState({ contentVisible: !this.state.contentVisible })
+        event.preventDefault()
+
+        if(this.state.contentVisible) {
+            this.setState(INITIAL_STATE) 
+        } else {
+            this.setState({ contentVisible: true })
+        }
     }
 
     render() {
@@ -53,7 +61,7 @@ class Card extends Component {
             {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
             {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
             {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
-            {title: 'Number', state: this.state.numberTwo, name: 'numbertwo'},
+            {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
 
             {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
             {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
@@ -67,16 +75,17 @@ class Card extends Component {
         ]
 
         return (
-            <form onSubmit={this.handleInputChange} className="card">
+            <form onSubmit={this.handleFormSubmit} className="card">
                 <div className="card__inputs">
-                    inputData.map(data => Input( (data), this.handleInputChange))
+                {
+                     inputDate.map((data, index) => {
+                         return Input( (data), this.handleInputChange, index ) 
+                    })
+                }
                 </div>
                 <button type="submit">{!this.state.contentVisible ? 'Generate Mad lib' : 'Clear Form'}</button>
                 {
-                   inputDate.map(data => Input( (data), this.handleInputChange  ))
-                }
-                {
-                    this.state.contentVisible ? <Content data={this.state}/> : <div>Hello</div>
+                    this.state.contentVisible ? <Content data={this.state}/> : ''
                 }
            </form>
 
